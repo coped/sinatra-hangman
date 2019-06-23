3a685a0324c90bc07dc7cf8f28dcc0ef948e823a
Dennis Cope <dennisaaroncope@gmail.com>
2019-06-23 04:35:00 -0700

    Remove global variable

--- app.rb
+++ app.rb
@@ -2,28 +2,28 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 require './public/hangman.rb'
 
-game = Game.new
+set :game, Game.new
 
 get '/' do
-    game = Game.new
+    settings.game = Game.new
     erb :index, layout: :main, :locals => {
-        :hangman => game.display_hangman,
-        :word_progress => game.word_progress,
+        :hangman => settings.game.display_hangman,
+        :word_progress => settings.game.word_progress,
     }
 end
 
 get '/play' do
     guess = params['guess'].strip.downcase
-    game.check_guess(guess)
-    word_reveal = "The word was #{game.secret_word}"
+    settings.game.check_guess(guess)
+    word_reveal = "The word was #{settings.game.secret_word}"
     erb :play, layout: :main, :locals => {
-        :word_progress => game.word_progress,
+        :word_progress => settings.game.word_progress,
         :word_reveal => word_reveal,
-        :over => game.over,
-        :header => game.get_header,
-        :hangman => game.display_hangman,
-        :lives_left => game.lives_left,
-        :incorrect_guesses => game.incorrect_guesses,
-        :secret_word => game.secret_word,
+        :over => settings.game.over,
+        :header => settings.game.get_header,
+        :hangman => settings.game.display_hangman,
+        :lives_left => settings.game.lives_left,
+        :incorrect_guesses => settings.game.incorrect_guesses,
+        :secret_word => settings.game.secret_word,
     }
 end
